5cf12d94a55e3d5c52f51ea66b3127cb2c933be8
jsdegithub <def_myself@qq.com>
2021-03-29 14:13:50 +0800

    配置css热更新

--- src/js/index.js
+++ src/js/index.js
@@ -4,15 +4,23 @@ import "../css/reset.css";
 import "../css/index.scss";
 import "../css/iconfont.css";
 
-var img = new Image();
+/* var img = new Image();
 img.src = avatar;
 img.classList.add("avatar");
 var app = document.getElementById("app");
-app.appendChild(img);
+app.appendChild(img); */
+
+var app = document.getElementById("app");
 
 var iconfontElement = document.createElement("div");
 iconfontElement.innerHTML = "<div class='iconfont icon-UserService'></div>";
-
 app.appendChild(iconfontElement);
+
+var btn = document.getElementsByTagName("button")[0];
+btn.onclick = function () {
+    var txtElement = document.createElement("p");
+    txtElement.innerHTML = "<h2>这是点击按钮生成的文本</h2>";
+    app.appendChild(txtElement);
+};
 
 header();
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { HotModuleReplacementPlugin } = require("webpack");
 
 module.exports = {
     mode: "development",
-    devtool: "source-map",
+    devtool: "cheap-module-eval-source-map", //生产环境下我们不生成sourcemap
     entry: "./src/js/index.js",
     output: {
         filename: "bundle.js",
@@ -12,12 +13,17 @@ module.exports = {
     },
     devServer: {
         contentBase: "./dist",
+        open: true,
+        port: 8080,
+        hot: true,
+        hotOnly: true, //热更新不生效也不让浏览器自动刷新
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/html/index.html",
         }),
         new CleanWebpackPlugin(["dist"]),
+        new HotModuleReplacementPlugin(),
     ],
     module: {
         rules: [
